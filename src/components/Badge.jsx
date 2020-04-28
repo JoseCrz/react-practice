09656bad8a515b3a6e9614d295995249997150f5
Jose Cuevas <josecueram@gmail.com>
2020-04-27 19:46:13 -0500

    created styles for Badge Component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 import logo from '../images/logo.svg'
 
+import '../styles/Badge.css'
+
 class Badge extends React.Component {
   render () {
     return (
-      <div className="">
-        <div className="">
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={logo} alt="Logo"/>
         </div>
-        <div className="">
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
           <h1>José<br/>Cuevas</h1>
         </div>
-        <div className="">
-          <p>Full Stack Developer</p>
+        <div className="Badge__section-info">
+          <h3>Full Stack Developer</h3>
           <a href="https://twitter.com/josecrz13">@josecrz13</a>
         </div>
-        <div className="">
+        <div className="Badge__footer">
           <p>#ConsumeXalapa</p>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Badge from './components/Badge'
 
+import './global.css'
 const container = document.getElementById('app');
 
 ReactDOM.render(< Badge />, container);
