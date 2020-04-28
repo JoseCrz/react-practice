86d8f58eca203b1a7083be9a85aea33a4b382cdb
Jose Cuevas <josecueram@gmail.com>
2020-04-27 21:05:07 -0500

    Created Hero Component CSS & JSX

--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 class BadgeNew extends React.Component {
   render () {
     return (
       <div>
         <Header />
+        <Hero />
       </div>
     )
   }
