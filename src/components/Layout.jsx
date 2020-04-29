89b0dade7ece487f607b79db0196036fd598542e
Jose Cuevas <josecueram@gmail.com>
2020-04-29 16:39:08 -0500

    minor tweak to Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
+import '../styles/Wrapper.css'
+
 function Layout (props) {
   return (
-    <React.Fragment>
+    <div className="Wrapper">
       <Header />
       {props.children}
       <Footer />
-    </React.Fragment>
+    </div>
   )
 }
 
