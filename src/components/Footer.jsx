5883f58521abb82e73e641cea4113830f9138f10
Jose Cuevas <josecueram@gmail.com>
2020-04-28 22:04:46 -0500

    minor fixes to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../styles/Footer.css'
 import '../styles/Wrapper.css'
@@ -7,15 +8,15 @@ class Footer extends React.Component {
   render () {
     return (
       <div className="Wrapper">
-        <footer class="Footer">
-          <div class="Footer__top">
-              <div class="Footer__container">
+        <footer className="Footer">
+          <div className="Footer__top">
+              <div className="Footer__container">
                   <p>¿Tienes alguna duda?</p>
-                  <a class="Footer__button" href="#">Contáctanos</a>
+                  <Link className="Footer__button" to="/">Contáctanos</Link>
               </div>
           </div>
-          <div class="Footer__bottom">
-              <p class="Footer__credits">Consume Xalapa todo los derechos reservados</p>
+          <div className="Footer__bottom">
+              <p className="Footer__credits">Consume Xalapa todo los derechos reservados</p>
           </div>
         </footer>
       </div>
