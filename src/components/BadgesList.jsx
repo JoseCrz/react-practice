742f63e0f2ab32302ec58ba32598db4f20649db9
Jose Cuevas <josecueram@gmail.com>
2020-04-28 23:59:13 -0500

    implented API call to simulated API

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../styles/BadgesList.css'
 
 class BadgesList extends React.Component {
   render () {
+    if (this.props.badges.length === 0) {
+      return (
+        <React.Fragment>
+          <h3>We couldn't find any badges</h3>
+          <p>Why don't you try creating a new one?</p>
+          <Link className="Badges__button" to="/badges/new">Create my first Badge</Link>
+        </React.Fragment>
+      )
+    }
     return (
       <ul className="BadgesList__list">
         {this.props.badges.map(badge => {
@@ -12,7 +22,7 @@ class BadgesList extends React.Component {
               <div className="BadgesList__divider">
                 <img src={badge.avatarUrl} alt="Avatar" className="BadgesList__image"/>
                 <div className="BadgesList__texts">
-                  <h4 className="BadgesList__name">{badge.name} {badge.lastname}</h4>
+                  <h4 className="BadgesList__name">{badge.firstName} {badge.lastName}</h4>
                   <a className="BadgesList__twitter" href="https://twitter.com/">@{badge.twitter}</a>
                   <p className="BadgesList__job">{badge.jobTitle}</p>
                 </div>
