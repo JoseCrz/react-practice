da729368b773c6a2f49f1d212988d4342c39b306
Jose Cuevas <josecueram@gmail.com>
2020-04-28 20:49:24 -0500

    Created Not Found

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Layout from './Layout'
 import BadgeNew from '../pages/BadgeNew'
 import Badges from '../pages/Badges'
+import NotFound from '../pages/NotFound'
 
 class App extends React.Component {
   render () {
@@ -13,6 +14,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/badges" component={Badges} />
             <Route exact path="/badges/new" component={BadgeNew} />
+            <Route component={NotFound} />
           </Switch>
         </Layout>
       </BrowserRouter>
