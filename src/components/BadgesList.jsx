f7877d6e77c098d1405906faee58e63e7ff7217b
Jose Cuevas <josecueram@gmail.com>
2020-05-04 00:37:11 -0500

    Added hooks to BadgesList to add filter functionality

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -4,21 +4,58 @@ import { Link } from 'react-router-dom'
 import Gravatar from '../components/Gravatar'
 
 import '../styles/BadgesList.css'
+import '../styles/Form.css'
 
-class BadgesList extends React.Component {
-  render () {
-    if (this.props.badges.length === 0) {
-      return (
-        <React.Fragment>
-          <h3>We couldn't find any badges</h3>
-          <p>Why don't you try creating a new one?</p>
-          <Link className="Badges__button" to="/badges/new">Create my first Badge</Link>
-        </React.Fragment>
-      )
-    }
+function useSearchBadges (badges) {
+  
+  const [query, setQuery] = React.useState('')
+  const [filteredBadges, setFilteredBadges] = React.useState(badges)
+  
+  React.useMemo(() => {
+    const results = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase())
+    })
+    
+    setFilteredBadges(results)
+  }, [badges, query])
+  
+  return {query, setQuery, filteredBadges}
+}
+
+function BadgesList (props) {
+  const badges = props.badges
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges)
+
+  if (filteredBadges.length === 0) {
     return (
+      <React.Fragment>
+        <div className="Form">
+          <label htmlFor="" className="Form__label">Filter them Badges</label>
+          <input className="Form__input" type="text"
+          value={query}
+          onChange={e => {
+            setQuery(e.target.value)
+          }} />
+        </div> 
+        <h3>We couldn't find any badges</h3>
+        <p>Why don't you try creating a new one?</p>
+        <Link className="Badges__button" to="/badges/new">Create my first Badge</Link>
+      </React.Fragment>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      <div className="Form">
+        <label htmlFor="" className="Form__label">Filter them Badges</label>
+        <input className="Form__input" type="text"
+        value={query}
+        onChange={e => {
+          setQuery(e.target.value)
+        }} />
+      </div> 
       <ul className="BadgesList__list">
-        {this.props.badges.map(badge => {
+        {filteredBadges.map(badge => {
           return (
             <li className="BadgesList__item" key={badge.id}>
               <Link to={`/badges/${badge.id}`} className="BadgesList__link">
@@ -35,8 +72,8 @@ class BadgesList extends React.Component {
           )
         })}
       </ul>
-    )
-  }
+    </React.Fragment>
+  )
 }
 
 export default BadgesList
